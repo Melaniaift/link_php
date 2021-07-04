7ed064880ed61cc8547ca525b7eb245b1d4f4c80
Melaniaift <ioanamelany99@yahoo.com>
2021-07-04 23:36:04 +0300

    link

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- SIngurul caz in care folosim get-->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <ul>
+            <li> <a href="<?php echo 'pag1.php?id=Popescu';?>">Popescu</a></li>
+            <li> <a href="<?php echo 'pag1.php?id=Ionescu';?>">Ionescu</a></li>
+            <li> <a href="<?php echo 'pag1.php?id=Georgescu';?>">Georgescu</a></li>
+            <li> <a href="<?php echo 'pag1.php?id=Mihaescu';?>">Mihaescu</a></li>
+        </ul>
+    </body>
+</html>
